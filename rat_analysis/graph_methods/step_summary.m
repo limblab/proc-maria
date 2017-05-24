6a04971851b570246ab37b23f9ac821999470172
maripeza <mkharp@gmail.com>
2017-05-24 16:45:42 -0500

    update calculation of height marker to ignore back swing

--- rat_analysis/graph_methods/step_summary.m
+++ rat_analysis/graph_methods/step_summary.m
@@ -14,7 +14,8 @@ load(kin_path);
 
 %next draw all traces for a trial, relative to the hip marker at zero
 figure(1); hold on;
-plot(rat.toe(:, 1)-rat.hip_bottom(:, 1), rat.toe(:, 2)-rat.hip_bottom(:, 2))
+rel_endpoint = rat.toe-rat.hip_bottom; 
+plot(rel_endpoint(:, 1), rel_endpoint(:, 2))
 
 %on that graph, plot the high/low and front/back points (for height and length)
 %deal with step splitting somehow??? um. drat. okay here goes. 
@@ -36,26 +37,18 @@ f_pks = rat.toe(f_locs, 1);
 %top ten and then sort them 
 sw_idx = [sort(f_locs(1:11)) sort(b_locs(1:11))];
 
-%TODO: um. figure out which section to limit the high values to. and low
-%values while i'm at it? not sure if i can do this broadly for all datasets
-%but it shouldn't be at the back of the step. so maybe eliminate all points
-%within a certain fraction of the step from the back of the step. but I
-%don't want to eliminate things that lift up from the back of the step,
-%right? huh. maybe deal with that special case if it comes up but for the
-%moments I don't need to deal with it I think. how to limit those peaks??
-
 %choose indices to exclude when determining high peaks - otherwise the back
 %swing of the foot gets included
 vals = []; 
-dist = 50;
+
 for i = 1:size(sw_idx, 1)-1
-    diffval = sw_idx(i, 2)-sw_idx(i+1, 1); 
-    vals = [vals sw_idx(i+1, 1)-diffval/2:sw_idx(i+1, 1)]; 
+    diffval = round((sw_idx(i+1, 1)-sw_idx(i, 2))/3, 0); 
+    vals = [vals sw_idx(i+1, 1)-diffval:sw_idx(i+1, 1)]; 
     %vals = [vals sw_idx(i, 2)-dist:sw_idx(i, 2)+dist]; 
 end
 idx = setdiff(1:size(rat.toe, 1), vals); 
 temp = rat.toe(:, 2); 
-temp(idx) = NaN; 
+temp(idx) = rat.toe(1, 2)-100; 
 
 figure; findpeaks(temp, 'SortStr', 'descend', 'MinPeakDistance', 20)
 title('High peaks'); 
@@ -67,8 +60,8 @@ title('Low peaks');
 l_pks = rat.toe(l_locs, 2); 
 
 
+hl_vals = [[sort(h_locs(1:10)); 0] sort(l_locs(1:11))];
 
-hl_vals = [sort(h_locs(1:11)) sort(l_locs(1:11))];
 %check this by plotting each full step, splitting the parts
 figure(100); 
 for step=1:size(sw_idx, 1)-1
@@ -84,9 +77,13 @@ for step=1:size(sw_idx, 1)-1
     plot(rat.toe(sw_idx(step, 2):sw_idx(step+1, 1), 1), rat.toe(sw_idx(step, 2):sw_idx(step+1, 1), 2), 'color', 'r'); 
 end
 
-
 %calculate and draw average trace of trial minus 1st and last step
-%
+%calculate avg high and low points, and avg front and back points
+steps = 2:size(sw_idx, 1)-1
+fr = mean(rat.toe(sw_idx(steps, 1)))
+bk = mean(rat.toe(sw_idx(steps, 2)))
+hi = mean(rat.toe(hl_vals(steps, 1)))
+lo = mean(rat.toe(hl_vals(steps, 2)))
 
 
 %save some shit down here probs
