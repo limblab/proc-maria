4a45a1907ee7c2dca48b5d4a217de27bb226b799
maripeza <mkharp@gmail.com>
2017-05-25 13:12:19 -0500

    more summary stat processing

--- rat_analysis/graph_methods/step_summary.m
+++ rat_analysis/graph_methods/step_summary.m
@@ -23,14 +23,14 @@ plot(rel_endpoint(:, 1), rel_endpoint(:, 2))
 %find the peaks, then pick the ten highest, invert, and pick ten lowest -
 %those are the high/low and front/back points in the steps.
 %Note: "front" goes negatively on the x axis
-figure; findpeaks(rat.toe(:, 1), 'SortStr', 'descend', 'MinPeakDistance', 80)
+figure; findpeaks(rel_endpoint(:, 1), 'SortStr', 'descend', 'MinPeakDistance', 80)
 title('Back peaks'); 
-[b_pks, b_locs] = findpeaks(rat.toe(:, 1), 'SortStr', 'descend', 'MinPeakDistance', 80);
-inv_arr = max(rat.toe(:, 1))*1.01 - rat.toe(:, 1); 
+[b_pks, b_locs] = findpeaks(rel_endpoint(:, 1), 'SortStr', 'descend', 'MinPeakDistance', 80);
+inv_arr = max(rel_endpoint(:, 1))*1.01 - rel_endpoint(:, 1); 
 figure; findpeaks(inv_arr, 'SortStr', 'descend', 'MinPeakDistance', 80)
 title('Forward peaks'); 
 [~, f_locs] = findpeaks(inv_arr, 'SortStr', 'descend', 'MinPeakDistance', 80);
-f_pks = rat.toe(f_locs, 1); 
+f_pks = rel_endpoint(f_locs, 1); 
 
 %front and back peak locations are also a good way to denote swing and
 %stance phases - swing starts at the back and goes forward - so pick the
@@ -46,18 +46,18 @@ for i = 1:size(sw_idx, 1)-1
     vals = [vals sw_idx(i+1, 1)-diffval:sw_idx(i+1, 1)]; 
     %vals = [vals sw_idx(i, 2)-dist:sw_idx(i, 2)+dist]; 
 end
-idx = setdiff(1:size(rat.toe, 1), vals); 
-temp = rat.toe(:, 2); 
-temp(idx) = rat.toe(1, 2)-100; 
+idx = setdiff(1:size(rel_endpoint, 1), vals); 
+temp = rel_endpoint(:, 2); 
+temp(idx) = rel_endpoint(1, 2)-100; 
 
 figure; findpeaks(temp, 'SortStr', 'descend', 'MinPeakDistance', 20)
 title('High peaks'); 
 [h_pks, h_locs] = findpeaks(temp, 'SortStr', 'descend', 'MinPeakDistance', 20);
-inv_arr = max(rat.toe(:, 2))*1.01 - rat.toe(:, 2); 
+inv_arr = max(rel_endpoint(:, 2))*1.01 - rel_endpoint(:, 2); 
 figure; findpeaks(inv_arr, 'SortStr', 'descend', 'MinPeakDistance', 20)
 title('Low peaks'); 
 [~, l_locs] = findpeaks(inv_arr, 'SortStr', 'descend', 'MinPeakDistance', 20);
-l_pks = rat.toe(l_locs, 2); 
+l_pks = rel_endpoint(l_locs, 2); 
 
 
 hl_vals = [[sort(h_locs(1:10)); 0] sort(l_locs(1:11))];
@@ -67,23 +67,30 @@ figure(100);
 for step=1:size(sw_idx, 1)-1
     subplot(5, 2, step); hold on;
     %plot stance
-    plot(rat.toe(sw_idx(step, 1):sw_idx(step, 2), 1), rat.toe(sw_idx(step, 1):sw_idx(step, 2), 2), 'color', 'b'); 
+    plot(rel_endpoint(sw_idx(step, 1):sw_idx(step, 2), 1), rel_endpoint(sw_idx(step, 1):sw_idx(step, 2), 2), 'color', 'b'); 
     %plot starting point
-    plot(rat.toe(sw_idx(step, 1), 1), rat.toe(sw_idx(step, 1), 2), 'o'); 
+    plot(rel_endpoint(sw_idx(step, 1), 1), rel_endpoint(sw_idx(step, 1), 2), 'o'); 
     %plot high and low points
-    plot(rat.toe(hl_vals(step, 1), 1), rat.toe(hl_vals(step, 1), 2), 'o'); 
-    plot(rat.toe(hl_vals(step, 2), 1), rat.toe(hl_vals(step, 2), 2), 'o'); 
+    plot(rel_endpoint(hl_vals(step, 1), 1), rel_endpoint(hl_vals(step, 1), 2), 'o'); 
+    plot(rel_endpoint(hl_vals(step, 2), 1), rel_endpoint(hl_vals(step, 2), 2), 'o'); 
     %plot swing
-    plot(rat.toe(sw_idx(step, 2):sw_idx(step+1, 1), 1), rat.toe(sw_idx(step, 2):sw_idx(step+1, 1), 2), 'color', 'r'); 
+    plot(rel_endpoint(sw_idx(step, 2):sw_idx(step+1, 1), 1), rel_endpoint(sw_idx(step, 2):sw_idx(step+1, 1), 2), 'color', 'r'); 
 end
 
 %calculate and draw average trace of trial minus 1st and last step
 %calculate avg high and low points, and avg front and back points
 steps = 2:size(sw_idx, 1)-1
-fr = mean(rat.toe(sw_idx(steps, 1)))
-bk = mean(rat.toe(sw_idx(steps, 2)))
-hi = mean(rat.toe(hl_vals(steps, 1)))
-lo = mean(rat.toe(hl_vals(steps, 2)))
+fr = rel_endpoint(sw_idx(steps, 1), 1);
+bk = rel_endpoint(sw_idx(steps, 2), 1);
+hi = rel_endpoint(hl_vals(steps, 1), 2);
+lo = rel_endpoint(hl_vals(steps, 2), 2);
+steplen = bk-fr; 
+avlen = mean(steplen); 
+stepht = hi-lo; 
+avht = mean(stepht); 
 
 
 %save some shit down here probs
+
+%save fr, bk, hi, lo step values so I can make summary figs with mean and
+%standard deviation
